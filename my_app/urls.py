1cee9108f0c5dfb2a1a8992c839864fe7884792f
madsunrise <rudnev.vanya@gmail.com>
2016-11-25 01:04:37 +0300

    List services and page with single servicr

--- my_app/urls.py
+++ my_app/urls.py
@@ -3,5 +3,6 @@ from my_app import views
 
 urlpatterns = [
     #url(r'^login/', views.login, name='login'),
+    url(r'^service/(?P<service_id>\d+)/$', views.service, name='service'),
     url(r'^$', views.index, name='index'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,30 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    s1 = {
+        'id': '21',
+        'title': u'Автомойка',
+        'body': u'Автомойка на углу Ботанической улицы, время работы 9-18 пн-вс'
+    }
+
+    s2 = {
+        'id': '22',
+        'title': u'Кафе быстрого питания',
+        'body': u'Приглашаем в наше кафе каждый день с 10 до 20 часов по адресу ул. Мясницкая, 22А'
+    }
+
+    services = [s1, s2]
+
+    return render(request, 'index.html', {'services': services})
+
+def service (request, service_id):
+    #service = get_object_or_404(Service, pk=question_id)
+    service = {
+        'id': '21',
+        'title': u'Автомойка',
+        'body': u'Автомойка на углу Ботанической улицы, время работы 9-18 пн-вс'
+    }
+    return render(request, 'service.html', {'service': service})
